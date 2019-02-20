d52694b18cfe093d44ed8c96dd667a1d06ee5286
Jahanzaibv2 <Jahanzaibv2@gmail.com>
2019-02-20 20:40:29 +0500

    .

---
+++ load.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Appzaib (https://ranajahanzaib.com)
+ * @copyright Rana Jahanzaib 2019
+ * 
+ * 
+ * Application Loader
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Rana Jahanzaib <Jahanzaibv2@gmail.com>
+ * 
+ * @package Open Console
+ */
